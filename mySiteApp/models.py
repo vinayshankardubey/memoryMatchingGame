65e03a97e1e8b83713469cd8d8d9a836ee2c6249
VinayDevs00 <vinaydevs00@gmail.com>
2021-09-13 17:57:57 +0530

    rest framework

--- mySiteApp/models.py
+++ mySiteApp/models.py
@@ -1,6 +1,2 @@
 from django.db import models
 
-# Create your models here.
-class GameLevels(models.Model):
-    firstName = models.CharField(max_length=10)
-    lastName = models.CharField(max_length=20)
